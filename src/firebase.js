2b9f2c342e7faaf85561034868bd00b795da2ccd
Afthab <afthabvu@gmail.com>
2022-10-03 20:05:19 +0530

    Project setup completed

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyChvWkf2DwTDyK-Qjo7ZHm48XIwWmDIo_o",
+  authDomain: "netflix-409d0.firebaseapp.com",
+  projectId: "netflix-409d0",
+  storageBucket: "netflix-409d0.appspot.com",
+  messagingSenderId: "676811964829",
+  appId: "1:676811964829:web:9ee21d2aea21f26dcdef74"
+};
