ce52e9d4e79e0c5b40168a16edcf44014c7926e7
Afthab <afthabvu@gmail.com>
2022-10-03 22:14:45 +0530

    Navbar completed

---
+++ src/components/homscreen/HomeScreen.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Nav from '../Nav/Nav'
+import "./HomeScreen.css"
+
+function HomeScreen() {
+  return (
+    <div className='homeScreen'>
+       <Nav />
+        {/* banner */}
+        {/* rows */}
+    </div>
+  )
+}
+
+export default HomeScreen
